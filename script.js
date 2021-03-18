b201d68abefb14a1a6e90509c60811560ed8d206
AnastasiaSharieva <67286934+AnastasiaSharieva@users.noreply.github.com>
2021-03-18 20:50:43 +0200

    style

---
+++ script.js
@@ -0,0 +1,9 @@
+// let list_ = list.querySelectorAll('dt');
+
+// console.log(list_);
+
+// for (let dt of list_) {
+// let but_plus = document.createElement('button');
+// but_plus.classList.add('plus');
+// dt.prepend(but_plus);
+// }
